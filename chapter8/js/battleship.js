f9fa9193ec67fcfd907fec490890cae658efe1dc
Robson Fernandes <robfds@gmail.com>
2016-10-12 05:56:32 -0300

    Finishing view test code

--- chapter8/js/battleship.js
+++ chapter8/js/battleship.js
@@ -1,14 +1,24 @@
 var view = {
   displayMessage: function(msg) {
     var messageArea = document.getElementById("messageArea");
-    messageArea.innerHTML = msg;
+    messageArea.innerHTML = "<h2>" + msg + "</h2>";
   },
 
   displayHit: function(location) {
-    // Not implemented
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
   },
 
   displayMiss: function(location) {
-    // Not implemented
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "miss")
   },
 }
+
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+view.displayMessage("Tap tap, is this thing on?");
