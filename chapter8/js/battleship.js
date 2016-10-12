60d993dbbc504a11bf9dfc2b6fdf673dc735105b
Robson Fernandes <robfds@gmail.com>
2016-10-12 05:26:49 -0300

    Chapter 8: Finishing deployment of view object

---
+++ chapter8/js/battleship.js
@@ -0,0 +1,14 @@
+var view = {
+  displayMessage: function(msg) {
+    var messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+  },
+
+  displayHit: function(location) {
+    // Not implemented
+  },
+
+  displayMiss: function(location) {
+    // Not implemented
+  },
+}
