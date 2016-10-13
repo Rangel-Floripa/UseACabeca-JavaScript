2d666d6ab5d9b8466d0789d8821809bca055bc8d
Robson Fernandes <robfds@gmail.com>
2016-10-13 19:06:27 -0300

    Implementing model object

--- chapter8/js/battleship.js
+++ chapter8/js/battleship.js
@@ -13,12 +13,47 @@ var view = {
     var cell = document.getElementById(location);
     cell.setAttribute("class", "miss")
   },
-}
+};
 
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
-view.displayMessage("Tap tap, is this thing on?");
+var model = {
+  boardSize: 7,
+  numShips: 3,
+  shiptLength: 3,
+  shipsSunk: 0,
+
+  ships: [
+    {locations: ["06", "16", "26"], hits: ["", "", ""]},
+    {locations: ["24", "34", "44"], hits: ["", "", ""]},
+    {locations: ["10", "11", "12"], hits: ["", "", ""]}
+  ],
+
+  isSunk: function(ship) {
+    for (var i = 0; i < this.shiptLength; i++) {
+      if (ship.hits[i] !== "hit") {
+        return false;
+      }
+    }
+    return true
+  },
+
+  fire: function(guess) {
+    console.log(guess)
+    for (var i = 0; i < this.numShips; i++) {
+      var ship = this.ships[i];
+      var index = ship.locations.indexOf(guess);
+      if (index >= 0) {
+        ship.hits[index] = "hit";
+        view.displayHit(guess);
+        view.displayMessage("HIT");
+        if (this.isSunk(ship)) {
+          view.displayMessage("You sank my battleship!");
+          this.shipsSunk++;
+        }
+        return true;
+      }
+    }
+    view.displayMiss(guess);
+    view.displayMessage("You missed");
+    return false;
+  }
+};
