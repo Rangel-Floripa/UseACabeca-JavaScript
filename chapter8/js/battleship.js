af4b8e5788b4daf4aed1c882faec655ec7b90b24
Robson Fernandes <robfds@gmail.com>
2016-10-16 18:31:35 -0200

    Chapter 8: implementing collision method to avoid ship collisions.

--- chapter8/js/battleship.js
+++ chapter8/js/battleship.js
@@ -81,6 +81,18 @@ var model = {
     return newShipLocations;
   },
 
+  collision: function(locations) {
+    for (var i = 0; i < this.numShips; i++) {
+      var ship = this.ships[i];
+      for (var j = 0; j < locations.length; i++) {
+        if (ship.locations.indexOf(locations[j]) >= 0) {
+          return true;
+        }
+      }
+    }
+    return false;
+  },
+
   allShipsIsSunk: function() {
     if (this.shipsSunk === this.numShips){
       return true;
