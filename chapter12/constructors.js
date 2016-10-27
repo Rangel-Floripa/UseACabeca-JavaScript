ef5ada72392f4d40495aed3c98185e003c248eea
Robson Fernandes <robfds@gmail.com>
2016-10-27 06:35:32 -0300

    Chapter 12: create some coffee orders...

--- chapter12/constructors.js
+++ chapter12/constructors.js
@@ -9,5 +9,26 @@ function Dog(name, breed, weight) {
     } else {
       alert(this.name.concat(" says Yip!"));
     }
+  };
+}
+
+function Coffee(roast, ounces) {
+  this.roast = roast;
+  this.ounces = ounces;
+
+  this.getSize = function() {
+    var size;
+    if (this.ounces === 8) {
+      size = "small"
+    } else if (this.ounces === 12){
+      size = "medium";
+    } else {
+      size = "large";
+    }
+    return size;
+  }
+
+  this.toString = function() {
+    return "You've ordered a ".concat(this.getSize(), " ", roast, " coffee.");
   }
 }
